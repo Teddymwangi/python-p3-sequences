efdc464f9023184e817b4aecc0c52c8236a1f4a4
Teddy mwangi <teddy.maina@student.moringaschool.com>
2024-04-01 20:53:37 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3
-
-def print_fibonacci(length):
-    pass
+def generate_fibonacci(length):
+    """Generate Fibonacci sequence up to the given length."""
+    if length == 1:
+        return [0]
+    elif length == 2:
+        return [0, 1]
+    else:
+        fib_sequence = [0, 1]
+        while len(fib_sequence) < length:
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
